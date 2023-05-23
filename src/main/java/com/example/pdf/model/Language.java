e68558f1fedc5be978446b25d46c892e4c88ad9d
Bronx666 <Hristyuk@outlook.com>
2023-05-23 23:08:47 +0100

    initial commit

--- src/main/java/com/example/pdf/model/Language.java
+++ src/main/java/com/example/pdf/model/Language.java
@@ -6,7 +6,7 @@ public enum Language {
     POLISH("pl");
     public final String label;
 
-    private Language(String label) {
+    Language(String label) {
         this.label = label;
     }
 }
--- src/main/java/com/example/pdf/model/UserRepository.java
+++ src/main/java/com/example/pdf/model/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.pdf.model;
 
-import com.example.pdf.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
